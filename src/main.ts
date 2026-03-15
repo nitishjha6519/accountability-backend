869dbd0dd26af51f92a2d1eeb601948df4892d29
nitish6519 <nitishjha6519@gmail.com>
2026-03-15 20:32:01 +0530

    build

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ExpressAdapter } from '@nestjs/platform-express';
 import { Request, Response, NextFunction } from 'express';
-// eslint-disable-next-line @typescript-eslint/no-require-imports
-const express = require('express');
 
-const server = express();
-
-async function bootstrap(): Promise<ReturnType<typeof express>> {
-  const app = await NestFactory.create(AppModule, new ExpressAdapter(server));
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
 
   // Log each incoming request URL
   app.use((req: Request, res: Response, next: NextFunction) => {
@@ -22,27 +17,6 @@ async function bootstrap(): Promise<ReturnType<typeof express>> {
     credentials: true,
   });
 
-  await app.init();
-
-  if (process.env.VERCEL !== '1') {
-    await app.listen(process.env.PORT ?? 3002);
-  }
-
-  return server;
+  await app.listen(process.env.PORT ?? 3002);
 }
-
-// Vercel serverless handler
-let cachedApp: ReturnType<typeof express> | null = null;
-
-export default async function handler(req: Request, res: Response) {
-  if (!cachedApp) {
-    cachedApp = await bootstrap();
-  }
-  // eslint-disable-next-line @typescript-eslint/no-unsafe-call
-  cachedApp(req, res);
-}
-
-// Local dev: run as normal server
-if (process.env.VERCEL !== '1') {
-  bootstrap();
-}
+bootstrap();
